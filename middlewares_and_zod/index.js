48d42cd472e89d650050dfe56d27bef0e580c2e6
Devil200207 <74138871+Devil200207@users.noreply.github.com>
2024-03-21 17:27:09 +0530

    get to know about zod and how its work

--- middlewares_and_zod/index.js
+++ middlewares_and_zod/index.js
@@ -1,5 +1,9 @@
 const express= require("express");
+const zod = require("zod");
 const app = express(); 
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads
+
 
 function Uservalidation(req,res,next)
 {
@@ -33,12 +37,44 @@ function KidneyValidator(req,res,next)
     }
 }
 
-app.get("/health-chekup",Uservalidation,KidneyValidator,(req,res) => {
+app.get("/health-chekup",Uservalidation,(req,res) => {
 
     res.status(200).json({
         msg:"User kidney is fine"
     });
     return; 
+})
+
+app.post("/health-kidney",(req,res) => {
+
+    const schema = zod.array(zod.number());
+  const kidneys = req.body.kidneys;
+  const response = schema.safeParse(kidneys);
+
+  res.send({
+    response
+  })
+})
+
+app.post("/user-validator",(req,res) => {   //user validation using zod
+
+    const schema = zod.object({
+        username:zod.string().email(),
+        password:zod.string().min(5)
+    })
+
+    const responce = schema.safeParse(req.body);
+    
+    if(!responce.success)
+    {
+        res.status(500).json({
+            msg:"Invalid User"
+        });
+    }
+    else{
+        res.send(responce)
+    }
+    
 })
 
 app.use((err,res,req,next) =>   //error handling middleware
